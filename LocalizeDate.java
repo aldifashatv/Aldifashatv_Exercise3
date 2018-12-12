773a7eb3fa3bbb2af878733ff2c34a14fb779102
Aldifashatv <aldifashatv@gmail.com>
2018-12-12 16:07:41 +0700

    Chinese deleted from the options

---
+++ LocalizeDate.java
@@ -0,0 +1,10 @@
+swtich (line) {
+	case "1": setEnglish(); run();
+	break:
+	case "2": setFrench(); run();
+	break;
+	case "3": setChinese(); run();
+	break;
+	case "4": setItalian(); run();
+	break;
+)
